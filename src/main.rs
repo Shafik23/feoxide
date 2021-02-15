8e8ab40dc156b7b83cb8f1dfd9613b2a527ba513
Shafik Amin <shafik23@gmail.com>
2021-02-14 17:45:44 -0700

    Added quit logic.

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,31 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
-
-    println!("Please input your guess:");
-
     let secret_number: u32 = rand::thread_rng().gen_range(1..100);
+
+    // println!("The secret number is {}", secret_number);
+
     let mut guess = String::new();
 
-    println!("The secret number is {}", secret_number);
+    loop {
+        guess.clear();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        println!();
+        println!("Please input your guess:");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You got it!"),
+        let guess: u32 = guess.trim().parse().expect("Please type a number!!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You got it!");
+                break;
+            }
+        }
     }
 }
